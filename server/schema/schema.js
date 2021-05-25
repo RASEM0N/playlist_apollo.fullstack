a9279e9e7bbcfce742b00eb6e19125290511c727
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-05-25 15:21:46 +0500

    more on the root queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -84,6 +84,18 @@ const RootQuery = new GraphQLObjectType({
                 return lodash.find(authors, { id: args.id })
             },
         },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return books
+            },
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args) {
+                return authors
+            },
+        },
     },
 })
 
